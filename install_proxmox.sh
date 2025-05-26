796e3120bfb4f79797bc9d0f197b3bc3d589cc94
Eirik Wulff <ew@dibk.no>
2025-05-26 14:01:42 +0200

    Added install script for proxmox, using python venv

---
+++ install_proxmox.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#root check
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+path="/opt/lcdInfo"
+mkdir -p ${path}
+cp -f lcdInfo.py requirements.txt ${path}/
+python -m venv ${path}
+
+cd ${path}
+source ${path}/bin/activate
+python -m pip install -r requirements.txt
+chmod +x lcdInfo.py
+
+
+# Setup service
+sed -i -e "s|/usr/local/bin/lcdInfo/lcdInfo.py|$path/bin/python $path/lcdInfo.py|g"
+# cat <<EOF > lcdInfo.service
+# [Unit]
+# Description=Qnap LCD Info Service
+# After=syslog.target
+
+# [Install]
+# WantedBy=multi-user.target
+
+# [Service]
+# Type=simple
+# Restart=on-failure
+# EOF
+# echo "ExecStart=${path}/bin/python ${path}/lcdInfo.py" >> lcdInfo.service
+# Install service
+mv systemd/lcdinfo.service /etc/systemd/system/
+systemctl enable lcdinfo.service
+systemctl start lcdinfo.service 
