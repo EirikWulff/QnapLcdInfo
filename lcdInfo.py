61df025b66986cfa5e29ab81bfefaeadf30adc85
Eirik Wulff <ew@dibk.no>
2025-05-28 13:33:00 +0200

    Better mount displaynames

--- lcdInfo.py
+++ lcdInfo.py
@@ -9,6 +9,9 @@ t=None
 load_dotenv()
 blankLcdTimeout=int(os.getenv('SCREEN_TIMEOUT', 10))
 
+def begins_with_one_of(text, substring)
+        return any(text.begins_with(substring))
+
 def getDataArray(network_regex="^eth|^enp|^bond|^vmbr"):
         output = []
         output.append([socket.gethostname(),"Load(5m): "+ str(psutil.getloadavg()[1])])
@@ -17,21 +20,18 @@ def getDataArray(network_regex="^eth|^enp|^bond|^vmbr"):
 
         mountpoints = ["/"]
         additional_mountpoints = os.getenv('MOUNTPOINTS', None)
-        mountpoints_names = os.getenv("MOUNTPOINT_NAMES", None)
-        if mountpoints_names:
-                mountpoints_names = mountpoints_names.split('|')
         if additional_mountpoints:
                 additional_mountpoints = additional_mountpoints.split('|')
                 mountpoints += additional_mountpoints
         
-        mp_index = 0
         for mountpoint in mountpoints:
-                if len(mountpoints_names) > (mp_index + 1):
-                        display_name = mountpoints_names[mp_index] + " [" + mountpoint + "]"
+                if mountpoint == "/":
+                        display_name = os.getenv('ROOT_NAME', 'ROOT')
                 else:
-                        display_name = mountpoint
-                output.append([display_name, "Usage: " + str(psutil.disk_usage(mountpoint).percent) + "%"])
-                mp_index += 1
+                        display_name = mountpoint.split('/')[-1]
+                        if display_name not 'Storage':
+                                display_name = upper(display_name)
+                output.append([display_name + ": " + mountpoint, "Usage: " + str(psutil.disk_usage(mountpoint).percent) + "%"])
 
         networks = psutil.net_if_addrs()
         for network in networks:
