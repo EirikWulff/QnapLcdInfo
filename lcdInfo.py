ffb4aa35fff940a2074dd94d83b7ff041493cbb0
Eirik Wulff <ew@dibk.no>
2025-05-28 12:15:30 +0200

    forgotten +=

--- lcdInfo.py
+++ lcdInfo.py
@@ -16,7 +16,7 @@ def getDataArray(network_regex="^eth|^enp|^bond|^vmbr"):
 
         mountpoints = ["/"]
         if (len(additional_mountpoints) > 0):
-                mountpoints  additional_mountpoints
+                mountpoints += additional_mountpoints
         for mountpoint in mountpoints:
                 output.appent([mountpoint + ":", str(psutil.disk_usage(mountpoint).percent + "%")])
         # for disk in psutil.disk_partitions():
